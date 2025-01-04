a88082f5feff208d79fffee23b822ae6c9933d33
Wakiren <javipedre4@gmail.com>
2025-01-04 23:02:31 +0100

    Map (1st version)

---
+++ Handout4b/Assets/Level/Sprite Sheet Road defDef.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Sprite Sheet Road def" tilewidth="16" tileheight="16" tilecount="462" columns="22">
+ <image source="Sprite Sheet Road def.png" width="352" height="336"/>
+ <tile id="193" probability="0.1"/>
+ <tile id="194" probability="0.1"/>
+ <tile id="195" probability="0.2"/>
+ <tile id="196" probability="0.6"/>
+</tileset>
