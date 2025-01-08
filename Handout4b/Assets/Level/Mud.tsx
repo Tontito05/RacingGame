68a7e61e0cc70ec6f4055aaf4b825c0c3ecf2f1e
Wakiren <javipedre4@gmail.com>
2025-01-08 23:10:30 +0100

    Added mud in .tmx exported it to csv and some modifications on aseprite file

---
+++ Handout4b/Assets/Level/Mud.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Mud" tilewidth="16" tileheight="16" tilecount="462" columns="22">
+ <image source="../MiniPixelPack2/Levels/Sprite Sheet Road.png" width="352" height="336"/>
+</tileset>
