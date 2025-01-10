1fbad4390c1dca37845a365749392cd9795dba18
Wakiren <javipedre4@gmail.com>
2025-01-10 23:10:46 +0100

    Title Change

--- Handout4b/Source/Globals.h
+++ Handout4b/Source/Globals.h
@@ -40,4 +40,4 @@ enum update_status
 #define WIN_BORDERLESS		false
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC				true
-#define TITLE "Physics 2D Playground"
+#define TITLE "Rally Race Runner"
