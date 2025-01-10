31d0922bbe0e48ff3dc85f08fb08051fdbe264b4
Tontito05 <ton.casals@estudiantat.upc.edu>
2025-01-10 20:35:11 +0100

    IA

--- Handout4b/Source/IA.h
+++ Handout4b/Source/IA.h
@@ -8,93 +8,85 @@ public:
 	IA(ModulePhysics* physics, int x, int y, int width, int height, Module* listener, Texture2D texture, Group type)
 		: Car(physics, x, y, width, height, listener, texture, type)
 	{
-		//Set the gears
-		Gears.push_back({ 1,1 });
-		Gears.push_back({ 2,2 });
-		Gears.push_back({ 4,4 });
-		Gears.push_back({ 6,6 });
 
 		//Set the waypoints
-		waypoints.push_back({ 132,330 });
-		waypoints.push_back({ 213,333 }); 
-		waypoints.push_back({ 288,335 }); 
-		waypoints.push_back({ 360,325 }); 
-		waypoints.push_back({ 396,296 }); 
-		waypoints.push_back({ 334,271 }); 
-		waypoints.push_back({ 260,233 }); 
-		waypoints.push_back({ 227,183 });
-		waypoints.push_back({ 249,141 });
-		waypoints.push_back({ 330,107 }); 
-		waypoints.push_back({ 441,97 }); 
-		waypoints.push_back({ 536,134 }); 
-		waypoints.push_back({ 541,172 });
-		waypoints.push_back({ 506,252 });
-		waypoints.push_back({ 454,320 });
-		waypoints.push_back({ 399,370 });
-		waypoints.push_back({ 287,377 });
-		waypoints.push_back({ 235,395 });
-		waypoints.push_back({ 198,427 });
-		waypoints.push_back({ 193,484 });
-		waypoints.push_back({ 267,547 });
-		waypoints.push_back({ 391,596 });
-		waypoints.push_back({ 458,600 });
-		waypoints.push_back({ 556,576 });
-		waypoints.push_back({ 596,521 });
-		waypoints.push_back({ 560,477 });
-		waypoints.push_back({ 470,448 });
-		waypoints.push_back({ 430,406 });
-		waypoints.push_back({ 465,366 });
-		waypoints.push_back({ 540,355 });
-		waypoints.push_back({ 626,331 });
-		waypoints.push_back({ 715,278 });
-		waypoints.push_back({ 771,184 });
-		waypoints.push_back({ 846,147 });
-		waypoints.push_back({ 986,144 });
-		waypoints.push_back({ 1023,173 });
-		waypoints.push_back({ 1020,198 });
-		waypoints.push_back({ 946,233 });
-		waypoints.push_back({ 832,271 });
-		waypoints.push_back({ 750,311 });
-		waypoints.push_back({ 687,362 });
-		waypoints.push_back({ 650,417 });
-		waypoints.push_back({ 661,464 });
-		waypoints.push_back({ 702,484 });
-		waypoints.push_back({ 778,466 });
-		waypoints.push_back({ 832,440 });
-		waypoints.push_back({ 894,395 });
-		waypoints.push_back({ 970,325 });
-		waypoints.push_back({ 1024,295 });
-		waypoints.push_back({ 1080,326 });
-		waypoints.push_back({ 1122,371 });
-		waypoints.push_back({ 1146,423 });
-		waypoints.push_back({ 1115,487 });
-		waypoints.push_back({ 1070,541 });
-		waypoints.push_back({ 1051,610 });
+		waypoints.push_back({ 200, 349 });
+		waypoints.push_back({ 266, 348 });
+		waypoints.push_back({ 322, 330 });
+		waypoints.push_back({ 347, 300 });
+		waypoints.push_back({ 343, 256 });
+		waypoints.push_back({ 296, 211 });
+		waypoints.push_back({ 252, 197 });
+		waypoints.push_back({ 205, 176 });
+		waypoints.push_back({ 167, 148 });
+		waypoints.push_back({ 170, 113 });
+		waypoints.push_back({ 233, 69 });
+		waypoints.push_back({ 316, 60 });
+		waypoints.push_back({ 432, 59 });
+		waypoints.push_back({ 501, 91 });
+		waypoints.push_back({ 562, 109 });
+		waypoints.push_back({ 626, 90 });
+		waypoints.push_back({ 709, 54 });
+		waypoints.push_back({ 791, 47 });
+		waypoints.push_back({ 877, 54 });
+		waypoints.push_back({ 903, 89 });
+		waypoints.push_back({ 892, 123 });
+		waypoints.push_back({ 784, 164 });
+		waypoints.push_back({ 751, 193 });
+		waypoints.push_back({ 733, 249 });
+		waypoints.push_back({ 710, 273 });
+		waypoints.push_back({ 645, 272 });
+		waypoints.push_back({ 590, 271 });
+		waypoints.push_back({ 541, 300 });
+		waypoints.push_back({ 535, 358 });
+		waypoints.push_back({ 548, 416 });
+		waypoints.push_back({ 520, 483 });
+		waypoints.push_back({ 452, 527 });
+		waypoints.push_back({ 342, 544 });
+		waypoints.push_back({ 217, 550 });
+		waypoints.push_back({ 175, 576 });
+		waypoints.push_back({ 171, 621 });
+		waypoints.push_back({ 206, 657 });
+		waypoints.push_back({ 283, 685 });
+		waypoints.push_back({ 358, 695 });
+		waypoints.push_back({ 408, 692 });
+		waypoints.push_back({ 445, 647 });
+		waypoints.push_back({ 531, 635 });
+		waypoints.push_back({ 607, 650 });
+		waypoints.push_back({ 648, 678 });
+		waypoints.push_back({ 732, 690 });
+		waypoints.push_back({ 842, 671 });
+		waypoints.push_back({ 861, 635 });
+		waypoints.push_back({ 830, 585 });
+		waypoints.push_back({ 768, 583 });
+		waypoints.push_back({ 733, 555 });
+		waypoints.push_back({ 737, 506 });
+		waypoints.push_back({ 785, 450 });
+		waypoints.push_back({ 865, 403 });
+		waypoints.push_back({ 961, 376 });
+		waypoints.push_back({ 986, 335 });
+		waypoints.push_back({ 968, 301 });
+		waypoints.push_back({ 932, 301 });
+		waypoints.push_back({ 907, 328 });
+		waypoints.push_back({ 920, 371 });
+		waypoints.push_back({ 938, 410 });
+		waypoints.push_back({ 947, 457 });
+		waypoints.push_back({ 970, 480 });
+		waypoints.push_back({ 1010, 491 });
+		waypoints.push_back({ 1065, 507 });
+		waypoints.push_back({ 1116, 547 });
+		waypoints.push_back({ 1150, 585 });
+
 
 	}
 
-	//Player functions////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	//Check if it can change gears
-	bool CheckGear();
-	//Check if the gear should go back
-	void GearBack();
-	//Get the maximum velocity of the car
-	b2Vec2 GetMaxVel();
-
-	// Method to get the car's position
-	Vector2 GetPosition();
+	//IA functions////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Pattern to follow the finish line and the road
 	void Pattern();
 
 	// Always forward
 	void MoveAlwaysForward(float speed);
-
-	//Gears --> They set a maximum velocity to the car and also can be changed with G
-	std::vector<b2Vec2> Gears;
-	int GearChange = 0;
-	bool CanChangeGear = false;
 
 	int waypointsIndex = 0;
 	std::vector<b2Vec2> waypoints;
